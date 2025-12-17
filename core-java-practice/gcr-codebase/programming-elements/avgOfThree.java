49adec51443a5da2c433b4b9d1c60586d3462e55
shivam25869 <ssharma80589024@gmail.com>
2025-12-17 22:33:45 +0530

    [shivam]avgOfThree

---
+++ core-java-practice/gcr-codebase/programming-elements/avgOfThree.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class avgOfThree {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+
+    // now calculate the avg of the given numbers
+    int result = (a + b + c) / 3;
+
+    System.out.println("The Avg of given three number is ------->" + result);
+
+  }
+
+}
