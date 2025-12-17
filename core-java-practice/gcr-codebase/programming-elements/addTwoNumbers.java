1a547140680c12a7a5cc5dd7193fe0c0150fac54
shivam25869 <ssharma80589024@gmail.com>
2025-12-17 16:10:49 +0530

    [shivam]addTwoNumbers

---
+++ core-java-practice/gcr-codebase/programming-elements/addTwoNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class addTwoNumbers {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+  //taking input from user
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+
+    //given result after adding it 
+    System.out.println(a+b);
+
+  }
+
+}
