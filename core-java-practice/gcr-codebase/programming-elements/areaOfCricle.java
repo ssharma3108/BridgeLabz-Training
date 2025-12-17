a46f5448b07014058388de94876907d97aa010a7
shivam25869 <ssharma80589024@gmail.com>
2025-12-17 16:21:05 +0530

    [shivam]areaOfCircle

---
+++ core-java-practice/gcr-codebase/programming-elements/areaOfCricle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class areaOfCricle {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int r = sc.nextInt(); // taking input radius
+
+    // now calculate the result using 3.14*r*r
+    double result = 3.14 * r * r;
+
+    System.out.println("The area of circle is --->" + result);
+
+  }
+
+}
