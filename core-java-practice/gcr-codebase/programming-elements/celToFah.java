7fdd43d19cb05a079d759dca703f5ca861b7a525
shivam25869 <ssharma80589024@gmail.com>
2025-12-17 16:16:05 +0530

    [shivam]celToFah

---
+++ core-java-practice/gcr-codebase/programming-elements/celToFah.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class celToFah {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int cel = sc.nextInt(); // taking input from the user
+
+    int result = (cel * 9 / 5) + 32; // now calculate the temperature using formula
+    System.out.println("The temperature after converting cel to fah is ---->" + result);
+
+  }
+
+}
