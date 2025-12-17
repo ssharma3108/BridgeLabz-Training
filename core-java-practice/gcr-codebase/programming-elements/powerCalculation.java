00d8eab0c653448ca2aedc4fce5d3af678e1601c
shivam25869 <ssharma80589024@gmail.com>
2025-12-17 16:50:53 +0530

    [shivam]powerCalculation

---
+++ core-java-practice/gcr-codebase/programming-elements/powerCalculation.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class powerCalculation {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double power = sc.nextInt();
+    double exponent = sc.nextInt();
+
+    // calculate the power using math.power built in function
+    double result = Math.pow(power, exponent);
+
+    System.out.println(result);
+
+  }
+
+}
