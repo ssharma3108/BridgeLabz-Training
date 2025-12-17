785b0cb4b72c85ed4e4dff61f565ad1f3000937e
shivam25869 <ssharma80589024@gmail.com>
2025-12-17 16:40:37 +0530

    [shivam]perimeterOfRectangle

---
+++ core-java-practice/gcr-codebase/programming-elements/perimeterOfRectangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class perimeterOfRectangle {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double length = sc.nextInt();
+    double width = sc.nextInt();
+
+    // calculate the perimeter using formula 2*(length+width)
+    double result = 2 * (length + width);
+
+    System.out.println(result);
+
+  }
+
+}
