30b162c09e2165f1072fabba5d12150dca673494
shivam25869 <ssharma80589024@gmail.com>
2025-12-18 10:55:50 +0530

    [shivam]Average of Sam's marks

---
+++ core-java-practice/gcr-codebase/programming-elements/avgOfSam.java
@@ -0,0 +1,16 @@
+
+public class avgOfSam {
+  public static void main(String[] args) {
+    int Math = 94;
+    int Physics = 95;
+    int Chemistry = 96;
+
+    // now calculate the average
+    double result = (Math + Physics + Chemistry) / 3;
+
+    // print the result
+    System.out.println("Sam's average mark in PCM is " + result);
+
+  }
+
+}
