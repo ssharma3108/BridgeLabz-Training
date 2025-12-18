9d17afa27c73cdba1baf783f99cdc4c323edaf4e
shivam25869 <ssharma80589024@gmail.com>
2025-12-18 10:51:25 +0530

    [shivam]Age of Harry

---
+++ core-java-practice/gcr-codebase/programming-elements/ageOfHarry.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ageOfHarry {
+   public ageOfHarry() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      System.out.println("Enter the age -:");
+      int var2 = var1.nextInt();
+      int var3 = 2024 - var2;
+      System.out.println("Harry's age in 2024 is " + var3);
+   }
+}
