57c4fd37e9624421058d471abb2a7e5156ed9470
Shivam Sharma <shivam.sharma_cs22@gla.ac.in>
2025-12-19 10:53:36 +0530

    [Shivam]Refactor. Update and rename simpleIntrest.java to SimpleIntrest.java

--- core-java-practice/gcr-codebase/programming-elements/SimpleIntrest.java
+++ core-java-practice/gcr-codebase/programming-elements/SimpleIntrest.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class simpleIntrest {
+public class SimpleIntrest {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     double pricipal = sc.nextInt();
