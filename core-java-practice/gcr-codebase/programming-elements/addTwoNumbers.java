b29968d65e4067303001ba5f01f94ea8f053b0d1
shivam25869 <ssharma80589024@gmail.com>
2025-12-19 10:36:10 +0530

    [shivam]Refactor. Modify the class name

--- core-java-practice/gcr-codebase/programming-elements/addTwoNumbers.java
+++ core-java-practice/gcr-codebase/programming-elements/addTwoNumbers.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-public class addTwoNumbers {
+public class AddTwoNumbers {
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
   //taking input from user
     int a=sc.nextInt();
     int b=sc.nextInt();
 
-    //given result after adding it 
+    //given result after adding it
     System.out.println(a+b);
 
   }
