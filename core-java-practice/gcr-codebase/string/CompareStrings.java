a80cdf6f38bbb23255678a5701ce7108f83e5596
shivam25869 <ssharma80589024@gmail.com>
2025-12-22 10:56:55 +0530

    [shivam]Refactor. update the logic of compare two string by using loop and charAt

--- core-java-practice/gcr-codebase/string/CompareStrings.java
+++ core-java-practice/gcr-codebase/string/CompareStrings.java
@@ -7,8 +7,29 @@ public class CompareStrings {
     String a = sc.next();
     String b = sc.next();
 
-    //print whether these are equal or not 
-    System.out.println(a.equals(b));
+    boolean isEqual = true;
+
+    // print whether these are equal or not
+
+    // check for base condition
+    if (a.length() != b.length()) {
+      isEqual = false;
+
+    } else {
+
+      for (int i = 0; i < a.length(); i++) {
+        if (a.charAt(i) != b.charAt(i)) {
+          isEqual = false;
+          break;
+        }
+      }
+    }
+    // now check and print the output
+    if (isEqual) {
+      System.out.println("The given string are equal");
+    } else {
+      System.out.println("The given string are not equal");
+    }
 
   }
 }
