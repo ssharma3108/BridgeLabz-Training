1bf92cfaeccc2430a54aec96c0105e6797dfcfd9
shivam25869 <ssharma80589024@gmail.com>
2025-12-22 10:34:59 +0530

    [shivam]Add. Compare Strings

---
+++ core-java-practice/gcr-codebase/string/CompareStrings.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class CompareStrings {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // taking input string
+    String a = sc.next();
+    String b = sc.next();
+
+    //print whether these are equal or not 
+    System.out.println(a.equals(b));
+
+  }
+}
