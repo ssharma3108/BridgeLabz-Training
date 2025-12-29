83260a6d96f907afc4bc787227e4d2f543b25707
shivam25869 <ssharma80589024@gmail.com>
2025-12-29 11:36:15 +0530

    [shivam]Add. Fibonacci series

---
+++ core-java-practice/gcr-codebase/extras/Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+  // using recursion for find the fibonacci sequence.........
+  static int fibo(int n) {
+    if (n == 0) {
+      return 0;
+    } else if (n == 1) {
+      return 1;
+    }
+    return fibo(n - 1) + fibo(n - 2);
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    System.out.println(fibo(n));
+  }
+}
