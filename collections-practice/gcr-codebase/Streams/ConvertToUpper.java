ddf480123bef3945ac18050cb196f12049d70d6d
shivam25869 <ssharma80589024@gmail.com>
2026-01-26 18:27:32 +0530

    [Shivam]Add. Convert to Upper

---
+++ collections-practice/gcr-codebase/Streams/ConvertToUpper.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class COnvertToUpper {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"));
+
+            String str;
+            while ((str = br.readLine()) != null) {
+                bw.write(str.toUpperCase());
+                bw.newLine();
+            }
+
+            br.close();
+            bw.close();
+
+            System.out.println("Operation Done");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
