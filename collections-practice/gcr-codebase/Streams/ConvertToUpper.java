3d30c3ce4d0bd785bb5e607fe425b490337ca339
shivam25869 <ssharma80589024@gmail.com>
2026-01-26 18:28:14 +0530

    [Shivam]Refactor. Update File Name

--- collections-practice/gcr-codebase/Streams/ConvertToUpper.java
+++ collections-practice/gcr-codebase/Streams/ConvertToUpper.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class COnvertToUpper {
+public class ConvertToUpper {
     public static void main(String[] args) {
         try {
             BufferedReader br = new BufferedReader(new FileReader("data.txt"));
