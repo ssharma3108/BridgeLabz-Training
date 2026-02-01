8e47bd95900c0a20991f5d241be9511fdd93c39c
shivam25869 <ssharma80589024@gmail.com>
2026-02-01 19:38:56 +0530

    [Shivam]Refactor. Update the code of all files

--- collections-practice/gcr-codebase/Junit/junit/src/main/java/junit_testing/junit/LongTask.java
+++ collections-practice/gcr-codebase/Junit/junit/src/main/java/junit_testing/junit/LongTask.java
@@ -2,4 +2,8 @@ package junit_testing.junit;
 
 public class LongTask {
 
+    public String longRunningTask() throws InterruptedException {
+        Thread.sleep(3000); 
+        return "Done";
+    }
 }
