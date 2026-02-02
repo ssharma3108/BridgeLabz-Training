8f5b6c21d937a94c002b7b5fcbc1699c7f828b4f
shivam25869 <ssharma80589024@gmail.com>
2026-02-02 15:47:30 +0530

    [Shivam]Add . CSV filter

--- io-programming-practice/gcr-codebase/csvHandling/CSV/src/csvDataHandling/CSVFilterRecord.java
+++ io-programming-practice/gcr-codebase/csvHandling/CSV/src/csvDataHandling/CSVFilterRecord.java
@@ -1,5 +1,34 @@
 package csvDataHandling;
 
 public class CSVFilterRecord {
+	public static void main(String[] args) {
 
+        try (BufferedReader br =
+                     new BufferedReader(new FileReader("students.csv"))) {
+
+            String line;
+
+            br.readLine(); // skip header
+
+            while ((line = br.readLine()) != null) {
+
+                String[] data = line.split(",");
+
+                int marks = Integer.parseInt(data[3]);
+
+                if (marks > 80) {
+
+                    System.out.println(
+                            data[0] + " " +
+                            data[1] + " " +
+                            data[2] + " " +
+                            data[3]
+                    );
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
