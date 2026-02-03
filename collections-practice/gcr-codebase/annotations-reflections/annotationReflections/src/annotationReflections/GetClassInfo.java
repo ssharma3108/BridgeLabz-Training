9c554c08327656e2702eaa6b1df365a54fc2bb4c
shivam25869 <ssharma80589024@gmail.com>
2026-02-03 11:00:45 +0530

    [Shivam]Add . GetClassInfo

--- collections-practice/gcr-codebase/annotations-reflections/annotationReflections/src/annotationReflections/GetClassInfo.java
+++ collections-practice/gcr-codebase/annotations-reflections/annotationReflections/src/annotationReflections/GetClassInfo.java
@@ -1,5 +1,33 @@
 package annotationReflections;
 
-public class GetClassInfo {
+import java.lang.reflect.Method;
 
+class Info {
+
+    String nameString;
+
+    public Info(String nameString) {
+        this.nameString = nameString;
+    }
+
+    public void callMethod() {
+        System.out.println("Method called Successfully");
+    }
+}
+
+public class GetClassInfo {
+    public static void main(String[] args) {
+
+        Info info = new Info("Reflection Demo");
+
+       
+        Class<?> cls = info.getClass();
+
+       
+        Method[] methods = cls.getDeclaredMethods();
+
+        for (Method m : methods) {
+            System.out.println(m.getName());
+        }
+    }
 }
