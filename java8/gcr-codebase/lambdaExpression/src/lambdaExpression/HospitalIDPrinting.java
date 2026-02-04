7b88ff9054fc81eca281d14b983eafd1f0bb559b
shivam25869 <ssharma80589024@gmail.com>
2026-02-04 15:00:07 +0530

    [Shivam]Add .Hospital Id printing

---
+++ java8/gcr-codebase/lambdaExpression/src/lambdaExpression/HospitalIDPrinting.java
@@ -0,0 +1,23 @@
+package lambdaExpression;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class HospitalIDPrinting {
+public static void main(String[] args) {
+	List<Integer> ls=Arrays.asList(101, 102, 103, 104, 105);
+	
+	if(ls.isEmpty()) {
+		System.out.println("No patient IDs found for verification\r\n"
+				+ "");
+	}
+	
+	
+	//print all the id...
+	ls.stream().forEach(System.out::println);
+	
+	
+}
+}
