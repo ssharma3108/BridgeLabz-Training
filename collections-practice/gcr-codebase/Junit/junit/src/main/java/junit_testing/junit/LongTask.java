47f50a2e65999484bf81ebb9dcd5112be1eefb20
shivam25869 <ssharma80589024@gmail.com>
2026-02-08 12:29:00 +0530

    [Shivam]Add .

--- collections-practice/gcr-codebase/Junit/junit/src/main/java/junit_testing/junit/LongTask.java
+++
@@ -1,9 +0,0 @@
-package junit_testing.junit;
-
-public class LongTask {
-
-    public String longRunningTask() throws InterruptedException {
-        Thread.sleep(3000); 
-        return "Done";
-    }
-}
--- collections-practice/gcr-codebase/regex/ValidLiscense.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class ValidLiscense {
-  public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    String str=sc.next();
-
-    System.err.println(str.matches("[A-Z]{2}[0-9]{4}"));
-  }
-}
