ddac50af8005a33b8b34dd1556f802293835ac24
shivam25869 <ssharma80589024@gmail.com>
2026-02-11 11:10:42 +0530

    [Shivam]Add . Unit Testing

--- collections-practice/gcr-codebase/Junit/junit/src/test/java/junit_testing/junit/UnitTest.java
+++ collections-practice/gcr-codebase/Junit/junit/src/test/java/junit_testing/junit/UnitTest.java
@@ -1,5 +1,39 @@
 package junit_testing.junit;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UnitTest {
 
+    @Test
+    public void Test_Deposit_ValidAmount() throws Exception {
+        Program account = new Program(1000);
+        account.Deposit(500);
+        assertEquals(1500, account.getBalance());
+    }
+
+    @Test
+    public void Test_Deposit_NegativeAmount() {
+        Program account = new Program(1000);
+        Exception exception = assertThrows(Exception.class, () -> {
+            account.Deposit(-100);
+        });
+        assertEquals("Deposit amount cannot be negative", exception.getMessage());
+    }
+
+    @Test
+    public void Test_Withdraw_ValidAmount() throws Exception {
+        Program account = new Program(1000);
+        account.Withdraw(400);
+        assertEquals(600, account.getBalance());
+    }
+
+    @Test
+    public void Test_Withdraw_InsufficientFunds() {
+        Program account = new Program(1000);
+        Exception exception = assertThrows(Exception.class, () -> {
+            account.Withdraw(2000);
+        });
+        assertEquals("Insufficient funds.", exception.getMessage());
+    }
 }
