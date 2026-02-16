bff1d5c0a6c8c04af3ff46133000a699d22a9f32
shivam25869 <ssharma80589024@gmail.com>
2026-02-16 12:29:38 +0530

    [Shivam]Add . Employee wage problem

--- oops-practice/gcr-codebase/employeewagecomputationproblem/Operations.java
+++ oops-practice/gcr-codebase/employeewagecomputationproblem/Operations.java
@@ -1,57 +1,58 @@
 package employeewagecomputationproblem;
 
-import java.awt.Choice;
 import java.util.Random;
-
 
 public class Operations {
 
-public String checkAttendence() {
-	String [] attendenceStrings= {"Present","Absent"};
-	Random random=new Random();
-	
-	int length=random.nextInt(attendenceStrings.length);
-	
-	String choiceString =attendenceStrings[length];
-	
-	return choiceString;	
-}
+	private static final int FULL_TIME = 1;
+	private static final int PART_TIME = 2;
 
-//check the type of the employee
-public int typeOfEmployee(int choice) {
-	String[] typeStrings = {"FullTime", "PartTime"};
-	
-	if (choice == 1) {
-		System.out.println("Employee is " + typeStrings[0]);
-		return 1;
-	} else if (choice == 2) {
-		System.out.println("Employee is " + typeStrings[1]);
-		return 2;
-	} else {
-		System.out.println("Invalid Choice");
-		System.exit(0);
-		return 0; // unreachable but required
+	public void calculateWage(Company company) {
+
+		int totalHours = 0;
+		int totalDays = 0;
+		int totalWage = 0;
+
+		Random random = new Random();
+
+		while (totalHours < company.getMaxWorkingHours() &&
+			   totalDays < company.getWorkingDays()) {
+
+			totalDays++;
+
+			int empType = random.nextInt(3); // 0,1,2
+			int empHours = 0;
+
+			switch (empType) {
+			case FULL_TIME:
+				empHours = 8;
+				break;
+			case PART_TIME:
+				empHours = 4;
+				break;
+			default:
+				empHours = 0;
+			}
+
+			totalHours += empHours;
+
+			int dailyWage = empHours * company.getWagePerHour();
+			company.getDailyWageList().add(dailyWage);
+
+			totalWage += dailyWage;
+		}
+
+		company.setTotalWage(totalWage);
 	}
-}
 
-public void calculateWage() {
-	int wagePerHour = 20;
-	int wageFullHour = 8;
-	int partTimeHour = 8;   // (kept as you wrote)
-	int workingDays = 20;
-	
-	double total = 0;
-	
-	int type = typeOfEmployee(1);
-	
-	if (type == 1) {
-		total = wageFullHour * wagePerHour;
-	} else if (type == 2) {
-		total = wagePerHour * partTimeHour;
+	public int getTotalWageByCompanyName(String name, java.util.List<Company> list) {
+
+		for (Company company : list) {
+			if (company.getCompanyName().equalsIgnoreCase(name)) {
+				return company.getTotalWage();
+			}
+		}
+
+		return -1; // company not found
 	}
-	
-	System.out.println(total);
-}
-
-	
 }
